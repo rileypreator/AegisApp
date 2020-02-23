f813a1ca2672c92b54f7a135df50b704367405fc
Aaron Picker <pic18006@byui.edu>
2020-02-22 19:59:50 -0700

    Added EmergencyActivity, InformationActivity, InternetActivity and associated XML files

---
+++ app/src/main/java/com/example/aegisapplication/EmergencyActivity.java
@@ -0,0 +1,15 @@
+package com.example.aegisapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EmergencyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+    }
+
+
+}
