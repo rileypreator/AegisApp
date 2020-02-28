7e25f54823d4eec95061a41d38edf9f08bf93aae
emileestratford23 <emileestratford23@gmail.com>
2020-02-28 11:43:22 -0700

    Button to test

--- app/src/main/java/com/example/aegisapplication/MainActivity.java
+++ app/src/main/java/com/example/aegisapplication/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.aegisapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void aaronTestFunction(){
 
+    }
+
+    public void onEmergencyClick(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:9515959301"));
+        startActivity(intent);
     }
 }
