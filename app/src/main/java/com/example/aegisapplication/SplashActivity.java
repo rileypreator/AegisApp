efed1f4cff1696f6262434940fd0a02749546992
rileypreator <pre15001@byui.edu>
2020-02-28 13:01:45 -0700

    Added Splash Screen

---
+++ app/src/main/java/com/example/aegisapplication/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.aegisapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+//    Got some help from this site
+//    https://www.bignerdranch.com/blog/splash-screens-the-right-way/
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+}
