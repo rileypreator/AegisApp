b957736ded1f1df3f6f1105171ccf7bf087270b8
Aaron Picker <pic18006@byui.edu>
2020-03-01 09:35:39 -0700

    Added Incognito functionality which is useable by every Activity

--- app/src/main/java/com/example/aegisapplication/Incognito.java
+++ app/src/main/java/com/example/aegisapplication/Incognito.java
@@ -1,4 +1,21 @@
 package com.example.aegisapplication;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+
+import static androidx.core.content.ContextCompat.startActivity;
+
 public class Incognito {
+    Activity activity;
+
+    public Incognito(Activity activity){
+        this.activity = activity;
+    }
+
+    public void leaveApp(){
+        String url = activity.getString(R.string.incognito_web_address);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        activity.startActivity(intent);
+    }
 }
